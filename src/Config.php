3b3023e28ee3a9938adcecb9b33eb0256a084286
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-07-16 09:28:39 -0300

    START

--- src/Config.php
+++ src/Config.php
@@ -2,13 +2,13 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = '/mvc/public';
+    const BASE_DIR = '/refazendomvc/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
     const DB_DATABASE = 'test';
     CONST DB_USER = 'root';
-    const DB_PASS = '';
+    const DB_PASS = 'Amanda@230406';
 
     const ERROR_CONTROLLER = 'ErrorController';
     const DEFAULT_ACTION = 'index';
